2b7439ea6ba88be47c51005257733f64c2427f3c
RHernandez44 <razhanrahan@gmail.com>
2022-01-16 23:02:32 +1300

    Added Window size constants added Window instances

--- BIT504_AS3_5029791_Breakout/src/Breakout.java
+++ BIT504_AS3_5029791_Breakout/src/Breakout.java
@@ -4,6 +4,11 @@ import javax.swing.JFrame;
 
 public class Breakout extends JFrame{
 	
+	// Constant variables
+		private final static String WINDOW_TITLE = "Pong";
+	    private final static int WINDOW_WIDTH = 800;
+	    private final static int WINDOW_HEIGHT = 600;
+	
 	static final long serialVersionUID = 1L;
 	
 	private BreakoutPanel panel;
@@ -13,6 +18,14 @@ public class Breakout extends JFrame{
 		// TODO: Set the title
 		// TODO: Set the background colour to white
 		// TODO: Set resizable to false
+		
+		setTitle(WINDOW_TITLE); // Title bar text
+		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+		setResizable(false); // Allows minimise & maximise window
+		add(new PongPanel());
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE); // Can use HIDE_ON_CLOSE
+		
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         panel = new BreakoutPanel(this);
         add(panel);
